850dc338e3518c6856726314bea4f426183599a8
Arnd Klocker <arnd.klocker@hNow.io>
2025-12-18 14:16:28 +0100

    Update e-mail address from Benedikt

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -794,13 +794,13 @@ const HomePage: React.FC = () => {
                           +49 160 95169473
                         </a>
                         <a 
-                          href="mailto:benedikt.hochmuth@icloud.com"
+                          href="mailto:benedikt.hochmuth@healthiq.at"
                           className="flex items-center justify-center text-stone-200 hover:text-stone-50 transition-colors duration-200"
                         >
                           <svg className="w-5 h-5 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 8l7.89 4.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                           </svg>
-                          benedikt.hochmuth@icloud.com
+                          benedikt.hochmuth@healthiq.at
                         </a>
                       </div>
                     </div>
